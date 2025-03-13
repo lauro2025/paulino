c1f48a7662b2ae40fe997cf94ed994caed1b0851
lauropaulino <tinopaulino@hotmail.com>
2025-03-13 15:20:07 -0300

    reinogarotada

---
+++ src/main/java/com/lauro/paulino/PaulinoApplication.java
@@ -0,0 +1,13 @@
+package com.lauro.paulino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaulinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaulinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lauro/paulino/PaulinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauro.paulino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
